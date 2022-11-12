0c6f9678bbb2026e7c0084dd48d1d8126d6e7687
Dmitry Kulakov <atomeistee@gmail.com>
2022-11-12 23:48:50 +0300

    fix: description

--- Binary-Tree-Inorder-Traversal.ts
+++ Binary-Tree-Inorder-Traversal.ts
@@ -1,5 +1,5 @@
 /*
-  Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+  Given the root of a binary tree, return the inorder traversal of its nodes' values.
 */
 
 // Answer
