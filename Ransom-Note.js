f83cebf9d50567cbd575db227d779ad6420ac124
Dmitry Kulakov <atomeistee@gmail.com>
2023-06-01 20:00:00 +0300

    completed Ransom Note

---
+++ Ransom-Note.js
@@ -0,0 +1,45 @@
+/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+*/
+
+// Answer
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const map = {};
+  for (let char of magazine) {
+    map[char] ? map[char]++ : (map[char] = 1);
+  }
+  for (let char of ransomNote) {
+    if (!map[char]) return false;
+    map[char]--;
+  }
+  return true;
+};
